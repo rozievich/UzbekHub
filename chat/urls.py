56141fdf4bc673fb1bb0d63d2c457fc0caf50648
rozievich <oybekrozievich@gmail.com>
2025-09-20 19:15:18 +0500

    Fixed bugs and optimizing code

--- chat/urls.py
+++ chat/urls.py
@@ -3,13 +3,15 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     ChatRoomViewSet,
     RoomMemberViewSet,
-    MessageViewSet
+    MessageViewSet,
+    FileViewSet
 )
 
 router = DefaultRouter()
 router.register(r'rooms', ChatRoomViewSet, basename='chatroom')
 router.register(r'members', RoomMemberViewSet, basename="room_members")
 router.register(r'message', MessageViewSet, basename="messages")
+router.register(r'files', FileViewSet, basename="files")
 
 urlpatterns = [
     path('', include(router.urls)),
